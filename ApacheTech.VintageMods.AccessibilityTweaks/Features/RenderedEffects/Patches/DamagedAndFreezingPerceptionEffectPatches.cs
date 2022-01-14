61ea9484e40abcb5d25f4d89b35e5760a0a1d31e
ApacheTech Solutions <p_matthews@live.co.uk>
2022-01-14 17:27:11 +0000

    Added Damaged and Freezing Perception Effect Patch

---
+++ ApacheTech.VintageMods.AccessibilityTweaks/Features/RenderedEffects/Patches/DamagedAndFreezingPerceptionEffectPatches.cs
@@ -0,0 +1,24 @@
+﻿using ApacheTech.VintageMods.Core.Abstractions.Features;
+using ApacheTech.VintageMods.Core.Services.HarmonyPatching.Annotations;
+using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+#pragma warning disable IDE0051 // Remove unused private members
+
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedType.Global
+
+namespace ApacheTech.VintageMods.AccessibilityTweaks.Features.RenderedEffects.Patches
+{
+    [HarmonySidedPatch(EnumAppSide.Client)]
+    public class DamagedAndFreezingPerceptionEffectPatches : WorldSettingsConsumer<RenderedEffectSettings>
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(DamagedAndFreezingPerceptionEffect), "OnBeforeGameRender")]
+        private static bool Patch_DamagedAndFreezingPerceptionEffect_OnBeforeGameRender_Prefix()
+        {
+            return Settings.CameraShakeEnabled;
+        }
+    }
+}
